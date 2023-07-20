46b566517a770f89e69789ed649ef03fba82d423
Michael Rose <michael@rosenetwork.net>
2023-07-20 00:37:22 -0700

    added requires to some fields and fixed date to actually return not just calculate it

--- models/flight.js
+++ models/flight.js
@@ -5,22 +5,27 @@ const flightsSchema = new Schema({
 airline: {
     type: String,
     enum: ['American', 'Southwest', 'United'],
+    required: true,
 },
 airport:{
     type: String,
     enum: ['AUS', 'DFW', 'DEN', 'LAX', 'SAN'],
     default: 'DEN',
+    required: true,
 },
 flightNo: {
     type: Number,
     min: 10,
     max: 9999,
+    required: true,
+    unique: true
 },
 departs: {
    type: Date, 
    default: ()=>{
     let d = new Date();
     d.setDate(d.getDate()+365)
+    return d
    }
 },
 })
